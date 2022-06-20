68d7eaf73a91629d9182b76b9ce3dab55289c7eb
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-06-20 10:37:55 -0500

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -9,6 +9,8 @@ const sections = [
    {
 	title: "TIPOS DE logos",
 	rows: [
+	    {title: ".textpro create-a-summer-text-effect", rowId: ".textpro create-a-summer-text-effect " + args[0] + ""},
+	    {title: ".textpro create-realistic-golden", rowId: ".textpro create-realistic-golden " + args[0] + ""},
 	    {title: ".textpro neon-light-blackpink-logo", rowId: ".textpro neon-light-blackpink-logo " + args[0] + ""},
 	    {title: ".textpro blackpink-logo", rowId: ".textpro blackpink-logo " + args[0] + ""},
 	    {title: ".textpro create-blackpink-style", rowId: ".textpro create-blackpink-style " + args[0] + ""},
